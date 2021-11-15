6e3154c3be8bbfa70580e37ee741993f0c39694e
elinarnten <elin.arnten@hotmail.com>
2021-11-15 18:13:55 +0100

    added function for user name input

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-const username = prompt("Vad heter du?");
+
+window.onload = main;
+
+document.getElementById("fearHeights").style.display = 'none';
+
+function nameResult(){
+const result = document.getElementById("userName").value;
+document.getElementById("demo").innerHTML = 'Hej ' + result + '! Vad roligt att du ska följa med på vår guide!';
+document.getElementById("introduce").style.display = 'none';
+ 
+
+}
+
+function fearQuestion() {
+    const yes = document.getElementById("yes");
+    const no = document.getElementById("no");
+    
+    if (yes.clicked == true) {
+        document.getElementById("greetings").style.display = 'none';
+        
+    } else if (no.clicked == true) {
+
+    }
+
+}
