72b3ba1b6161bec5f409617a89c133243671a4b2
elinarnten <elin.arnten@hotmail.com>
2021-11-16 13:53:50 +0100

    uppdated the files and fixed function of the fuctions

--- main.js
+++ main.js
@@ -1,25 +1,55 @@
 
 window.onload = main;
+let countdown;
+let seconds = 10;
 
-document.getElementById("fearHeights").style.display = 'none';
+function main() {  
+    hide();
+    addEventListeners();
+}
 
-function nameResult(){
-const result = document.getElementById("userName").value;
-document.getElementById("demo").innerHTML = 'Hej ' + result + '! Vad roligt att du ska följa med på vår guide!';
-document.getElementById("introduce").style.display = 'none';
- 
+function hide() {
+   document.getElementById("fearHeights").style.display = 'none';
+   document.getElementById("noEnter").style.display = 'none';
+   document.getElementById("enterAcess").style.display = 'none';
 
 }
 
-function fearQuestion() {
-    const yes = document.getElementById("yes");
-    const no = document.getElementById("no");
+function addEventListeners() {
+    const submitName = document.getElementById("submit-name")
+    submitName.onclick = getNameResult;
+}
+
+function getNameResult(){
+    const result = document.getElementById("userName").value;
+    document.getElementById("demo").innerHTML = 'Hej ' + result + '! Vad roligt att du ska följa med på vår guide!'; 
+    document.getElementById("introduce").style.display = 'none'; 
+    document.getElementById("fearHeights").style.display = null; 
+}
+
+function fearOfHeightsAnswerYes() {
+    //const yes = document.getElementById("yes");
+    document.getElementById("greetings").style.display = 'none'; 
+    document.getElementById("noEnter").style.display = null;
     
-    if (yes.clicked == true) {
-        document.getElementById("greetings").style.display = 'none';
-        
-    } else if (no.clicked == true) {
-
-    }
 
 }
+function fearOfHeightsAnswerNo() {
+    document.getElementById("greetings").style.display = 'none'; 
+    document.getElementById("enterAcess").style.display = null;
+
+  
+}
+
+/*function setCountDown() {
+    if (!countdown) {
+        countdown = setInterval(handleTick, seconds);
+    }
+    else (seconds === 0) {
+        document.getElementById("greetings").style.display = null;
+    }
+}*/
+//hur startar man allt? 
+//hur döljer man visar man funktioner? 
+//Hur sätter man igång funktioner?
+//if satsen
