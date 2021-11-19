061b7fb158e69dce6c06d03adb2ac9d5d9f46ac6
elinarnten <elin.arnten@hotmail.com>
2021-11-19 12:39:53 +0100

    fixed the issues with the background

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
-
-
 window.onload = main;
 
+const marsRobotBackground = "url('IMG/mars-robot.jpg')";
+const planetMarsBackground = "url('IMG/planet-mars.jpg')";
+const planetMoonBackground = "url('IMG/planet-moon.jpg')";
+const earthBackground = "url('IMG/nasa-earth.jpg')";
 
 function main() {  
     hide();
@@ -13,8 +15,6 @@ function hide() {
    document.getElementById("noEnter").style.display = 'none';
    document.getElementById("enterAccess").style.display = 'none';
    document.getElementById("inSpace").style.display = 'none';
-   
-
 }
 
 function addEventListeners() {
@@ -30,79 +30,67 @@ function getNameResult(){
 }
 
 function fearOfHeightsAnswerYes() {
-    document.getElementById("greetings").style.display = 'none'; 
+    document.getElementById("greetings-text").style.display = 'none'; 
     document.getElementById("noEnter").style.display = null;
-    
+}
 
-}
 function fearOfHeightsAnswerNo() {
-    document.getElementById("greetings").style.display = 'none'; 
-    document.getElementById("enterAccess").style.display = null;
-  
+    document.getElementById("greetings-text").style.display = 'none'; 
+    document.getElementById("enterAccess").style.display = null; 
 }
+
 function selectPlanet() {
+    const background = document.getElementById('main-background');
+    background.style.backgroundImage = earthBackground;
     document.getElementById('inSpace').style.display = null;
-    document.getElementById('space-background').style.display = null;
     document.getElementById('select-planet').style.display = null;
-    document.getElementById('start-background').style.display = 'none';
     document.getElementById('enterAccess').style.display = 'none';
-    document.getElementById('moon-background').style.display = 'none';
     document.getElementById('planet-moon').style.display = 'none';
-    document.getElementById('mars-background').style.display = 'none';
     document.getElementById('planet-mars').style.display = 'none';
     document.getElementById('planet-mars-robot').style.display = 'none';
-    document.getElementById('mars-robot-background').style.display = 'none';
+    document.getElementById('robotRespond').style.display = 'none';
     document.getElementById('planet-pluto').style.display = 'none';
     
 }
 
 function getMars() {
+    const background = document.getElementById('main-background');
+    background.style.backgroundImage = planetMarsBackground;
     document.getElementById('planet-mars').style.display = null;
-    document.getElementById('mars-background').style.display = null;
-    document.getElementById('space-background').style.display = 'none';
+    
     document.getElementById('select-planet').style.display = 'none';
 }
-function getMarsRobot() {
-    document.getElementById('planet-mars-robot').style.display = null;
-    document.getElementById('mars-robot-background').style.display = null;
-    document.getElementById('mars-background').style.display = 'none';
-    document.getElementById('planet-mars').style.display = 'none';
-    const textInput = document.getElementById('submit-input')
-    textInput.onclick = getRobotAnswer;
 
+function getMarsRobot() {
+    const background = document.getElementById('main-background');
+    background.style.backgroundImage = marsRobotBackground;
+    document.getElementById('planet-mars-robot').style.display = null;
+    document.getElementById('planet-mars').style.display = 'none';
+    document.getElementById('next').style.display = null;
+    const result = document.getElementById('submit-input')
+    result.onclick = getRobotAnswer;
 }
+
 function getRobotAnswer() {
-    const result = document.getElementById('text-input').value;
+    const result = document.getElementById('text-input-robot').value;
     console.log(result);
+    const background = document.getElementById('main-background');
+    background.style.backgroundImage = marsRobotBackground;
+    document.getElementById('planet-mars-robot').style.display = 'none';
+    document.getElementById('robotRespond').style.display = null;
+    document.getElementById('next').style.display = null;
+
 }
 
 function getPluto() {
     document.getElementById('planet-pluto').style.display = null;
-    document.getElementById('mars-robot-background').style.display = 'none';
-    document.getElementById('space-background').style.display = 'none';
     document.getElementById('select-planet').style.display = 'none';
 }
 
 function getMoon() {
+    const background = document.getElementById('main-background');
+    background.style.backgroundImage = planetMoonBackground;
     document.getElementById('planet-moon').style.display = null;
-    document.getElementById('moon-background').style.display = null;
-    document.getElementById('space-background').style.display = 'none';
     document.getElementById('select-planet').style.display = 'none';
     document.getElementById('next').style.display = null;
 }  
-
-/*function rocketCountDown() {
-    for (var i = 10; i < 10; i--) {
-        if (i > 0) {
-            console.log(i);
-        }
-        else if (i < 0 ) {
-            console.log("STOP");
-        }
-    }
-}*/
-//hur startar man allt? 
-//hur döljer man visar man funktioner? 
-//Hur sätter man igång funktioner?
-//if satsen
-//klappa mars roboten
